6ad2be6821466044632de8aa2c7a5a7890497d13
Everton Almeida <oevertonalmeida@outlook.com>
2022-12-07 10:53:39 -0300

    versao 5

--- oi.py
+++ oi.py
@@ -6,3 +6,17 @@ if pergunta_cor == "vermelho":
     print("Essa é a minha cor preferida")
 else:
     print("essa cor é bonita")
+
+pergunta_matematica = input(str(f"{nome},voce sabe somar ou subtrair ?"))
+if pergunta_matematica == "somar":
+    prim_num = int(input(f"{nome}, digite o primeiro numero: "))
+    seg_num = int(input(f"{nome}, digite o segundo numero: "))
+    resultado = prim_num + seg_num
+    print(f"o resultado da sua soma é {resultado}")
+elif pergunta_matematica == "subtrair":
+    prim_num2 = int(input("digite o primeiro numero: "))
+    seg_num2 = int(input("digite o segundo numero: "))
+    resultado = prim_num2 - seg_num2
+    print(f"o resultado da subtração é {resultado}")
+else:
+    print(f"{nome} estude mais!")
