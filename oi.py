ea2b36231c22cf49f9b9b91f9cbbc77db82ec76e
Everton Almeida <oevertonalmeida@outlook.com>
2022-12-07 10:33:08 -0300

    VERSAO 4

--- oi.py
+++ oi.py
@@ -1,2 +1,8 @@
 nome = input(str("Qual seu nome ?"))
 print(f"oi {nome}") 
+
+pergunta_cor = input(str("Me fale uma cor: "))
+if pergunta_cor == "vermelho":
+    print("Essa é a minha cor preferida")
+else:
+    print("essa cor é bonita")
